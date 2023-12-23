73905d05a47dcf981d2cd8df15c70e898ccdc921
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-23 20:26:42 +0800

    用户信息表

--- JFourBlazor/BlazorLearnWebApp/Entity/UserEntity.cs
+++ JFourBlazor/BlazorLearnWebApp/Entity/UserEntity.cs
@@ -1,11 +1,23 @@
-﻿using System.ComponentModel;
+﻿using FreeSql;
+using System.ComponentModel;
 
 namespace BlazorLearnWebApp.Entity
 {
     [Description("用户信息表")]
-    public class UserEntity
+    public class UserEntity:BaseEntity<UserEntity,int>
     {
-
-
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string? UserName { get; set; }    
+        /// <summary>
+        /// 用户密码
+        /// </summary>
+        public string? Password { get; set; }
+        /// <summary>
+        /// 用户昵称
+        /// </summary>
+        public string? NickName { get; set; }
+        
     }
 }
