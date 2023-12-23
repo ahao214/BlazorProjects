8bf222de719b673aadcfbbbe74382de065ec2d2d
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-23 20:31:44 +0800

    菜单信息表

---
+++ JFourBlazor/BlazorLearnWebApp/Entity/MenuEntity.cs
@@ -0,0 +1,34 @@
+﻿using FreeSql;
+using System.ComponentModel;
+
+namespace BlazorLearnWebApp.Entity
+{
+    [Description("菜单信息表")]
+    public class MenuEntity:BaseEntity<MenuEntity,int>
+    {
+        [Description("菜单名称")]
+        /// <summary>
+        /// 菜单名称
+        /// </summary>
+        public string? MenuName { get; set; }
+
+        [Description("URL链接")]
+        /// <summary>
+        /// URL链接
+        /// </summary>
+        public string? Url { get; set;}
+
+        [Description("菜单图标")]
+        /// <summary>
+        /// 菜单图标
+        /// </summary>
+        public string? Icon {  get; set;}
+
+        [Description("父级菜单ID")]
+        /// <summary>
+        /// 父级菜单ID
+        /// </summary>
+        public int ParentId { get; set;}
+
+    }
+}
