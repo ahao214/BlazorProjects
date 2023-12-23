6a33174b2bb53cef345eed7d5194b3a73966a54f
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-23 20:28:02 +0800

    角色信息表

---
+++ JFourBlazor/BlazorLearnWebApp/Entity/RoleEntity.cs
@@ -0,0 +1,18 @@
+﻿using FreeSql;
+using System.ComponentModel;
+
+namespace BlazorLearnWebApp.Entity
+{
+    [Description("角色信息表")]
+    public class RoleEntity:BaseEntity<RoleEntity,int>
+    {
+        [Description("角色名")]
+        /// <summary>
+        /// 角色名
+        /// </summary>
+        public string? RoleName { get; set; }
+
+
+
+    }
+}
