e6ff19c61ce37e1b8ee91b6751a35bd3f63dfc76
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 22:08:39 +0800

    验证

--- NiuNan/Joker.LearnBlazor.Web/Models/Product.cs
+++ NiuNan/Joker.LearnBlazor.Web/Models/Product.cs
@@ -1,15 +1,19 @@
-﻿namespace Joker.LearnBlazor.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Joker.LearnBlazor.Web.Models
 {
     /// <summary>
     /// 商品表
     /// </summary>
     public class Product
     {
-        public int ProductId { get; set; }  
-
-        public string ProductName { get; set; } 
-
-        public string ThumbnailImage { get; set; }  
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "商品名称")]
+        public string ProductName { get; set; }
+        [Required]
+        [Display(Name = "商品图片")]
+        public string ThumbnailImage { get; set; }
 
 
 
