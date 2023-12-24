a7ae77b3420df76cc78ffe7e2ed9858043a3d922
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 15:05:13 +0800

    页面

--- JFourBlazor/BlazorLearnWebApp/Components/Layout/MainLayout.razor.cs
+++ JFourBlazor/BlazorLearnWebApp/Components/Layout/MainLayout.razor.cs
@@ -65,8 +65,8 @@ namespace BlazorLearnWebApp.Components.Layout
 
             Menus = new List<MenuItem>
         {
-            new() { Text = "返回组件库", Icon = "fa-fw fa-solid fa-house", Url = "components" },
-            new() { Text = "后台模拟器", Icon = "fa-fw fa-solid fa-desktop", Url = "layout-page" },
+            new() { Text = "首页", Icon = "fa-fw fa-solid fa-house", Url = "/" },
+            new() { Text = "用户管理", Icon = "fa-fw fa-solid fa-desktop", Url = "user" },
             new() { Text = "示例网页", Icon = "fa-fw fa-solid fa-laptop", Url = "layout-demo/text=Parameter1" }
         };
         }
