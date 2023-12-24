29a62f038ca606c1914a84c5229cdd6ca7b604ba
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 14:50:51 +0800

    MainLayout页面样式

--- JFourBlazor/BlazorLearnWebApp/Components/Layout/MainLayout.razor.cs
+++ JFourBlazor/BlazorLearnWebApp/Components/Layout/MainLayout.razor.cs
@@ -3,9 +3,8 @@ using Microsoft.AspNetCore.Components;
 
 namespace BlazorLearnWebApp.Components.Layout
 {
-    public partial class MainLayout<TItem> where TItem : class, new()
+    public sealed partial class MainLayout
     {
-
         private bool IsOpen { get; set; }
 
         private string? Theme { get; set; }
@@ -17,41 +16,71 @@ namespace BlazorLearnWebApp.Components.Layout
 
         private IEnumerable<MenuItem>? Menus { get; set; }
 
+        /// <summary>
+        /// 获得/设置 是否固定 TabHeader
+        /// </summary>
         [Parameter]
         public bool IsFixedTab { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否固定页头
+        /// </summary>
         [Parameter]
         public bool IsFixedHeader { get; set; } = true;
+
+        /// <summary>
+        /// 获得/设置 是否固定页脚
+        /// </summary>
         [Parameter]
         public bool IsFixedFooter { get; set; } = true;
+
+        /// <summary>
+        /// 获得/设置 侧边栏是否外置
+        /// </summary>
         [Parameter]
         public bool IsFullSide { get; set; } = true;
 
+        /// <summary>
+        /// 获得/设置 是否显示页脚
+        /// </summary>
         [Parameter]
         public bool ShowFooter { get; set; } = true;
 
+        /// <summary>
+        /// 获得/设置 是否开启多标签模式
+        /// </summary>
         [Parameter]
         public bool UseTabSet { get; set; } = true;
 
+        /// <summary>
+        /// OnInitializedAsync 方法
+        /// </summary>
+        /// <returns></returns>
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
 
+            // 模拟异步获取菜单数据
             await Task.Delay(10);
 
             Menus = new List<MenuItem>
-            {
-                new (){Text="首页",Icon="fa-fw fa-solid fa-house",Url="/"},
-                new (){Text="用户管理",Icon="fa-fw fa-solid fa-desktop",Url="user"},
-new (){Text="示例网页",Icon="fa-fw fa-solid fa-laptop",Url="layout-demo/text=Parameter1"},
-            };
-
+        {
+            new() { Text = "返回组件库", Icon = "fa-fw fa-solid fa-house", Url = "components" },
+            new() { Text = "后台模拟器", Icon = "fa-fw fa-solid fa-desktop", Url = "layout-page" },
+            new() { Text = "示例网页", Icon = "fa-fw fa-solid fa-laptop", Url = "layout-demo/text=Parameter1" }
+        };
         }
 
+        /// <summary>
+        /// 更新组件方法
+        /// </summary>
         public void Update() => StateHasChanged();
 
         private void ToggleDrawer()
         {
             IsOpen = !IsOpen;
         }
+
+
     }
 }
