92c965f26b81fabfd953cedb53e2f580acf237dd
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 20:37:44 +0800

    商品表

---
+++ NiuNan/Joker.LearnBlazor.Web/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Joker.LearnBlazor.Web.Models
+{
+    /// <summary>
+    /// 商品表
+    /// </summary>
+    public class Product
+    {
+        public int ProductId { get; set; }  
+
+        public string ProductName { get; set; } 
+
+        public string ThumbnailImage { get; set; }  
+
+
+
+    }
+}
