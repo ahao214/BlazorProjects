cd010e4f5a93f08d6be120f997535e93329c81a6
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 20:40:44 +0800

    商品接口

---
+++ NiuNan/Joker.LearnBlazor.Web/Repository/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using Joker.LearnBlazor.Web.Models;
+
+namespace Joker.LearnBlazor.Web.Repository
+{
+    /// <summary>
+    /// 商品接口
+    /// </summary>
+    public interface IProduct
+    {
+        void Add(Product model);
+        void Delete(int id);
+        void Update(Product model);
+        List<Product> GetList();
+
+    }
+}
