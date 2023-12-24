6c37d015fcf8b8ec04b76830e1985a5fff3ed5bb
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-24 20:54:27 +0800

    注入

--- NiuNan/Joker.LearnBlazor.Web/Program.cs
+++ NiuNan/Joker.LearnBlazor.Web/Program.cs
@@ -1,4 +1,5 @@
 using Joker.LearnBlazor.Web;
+using Joker.LearnBlazor.Web.Repository;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+// ×¢Èë
+builder.Services.AddSingleton<IProduct, ProductRepository>();
 
 await builder.Build().RunAsync();
