044eb89598512bf2729fc41f3d3033ffcb719b38
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-25 20:56:52 +0800

    统计各个分类商品的数量

--- NiuNan/Joker.LearnBlazor.WebService/Models/Product.cs
+++ NiuNan/Joker.LearnBlazor.WebService/Models/Product.cs
@@ -8,16 +8,19 @@ namespace Joker.LearnBlazor.WebService.Models
     /// </summary>
     public class Product
     {
-        [SugarColumn(IsPrimaryKey=true,IsIdentity =true)]
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
         public int ProductId { get; set; } = 0;
         [Required]
         [Display(Name = "商品名称")]
         public string ProductName { get; set; } = "";
 
         [Display(Name = "商品图片")]
-        public string ThumbnailImage { get; set; } = ""; 
+        public string ThumbnailImage { get; set; } = "";
         [Display(Name = "商品分类")]
         public int CategoryId { get; set; } = 0;
+
+        [Navigate(NavigateType.OneToOne, nameof(CategoryId))]
+        public Category Category { get; set; }
 
     }
 }
--- NiuNan/Joker.LearnBlazor.WebService/Repository/ProductRepositoryMssql.cs
+++ NiuNan/Joker.LearnBlazor.WebService/Repository/ProductRepositoryMssql.cs
@@ -5,14 +5,40 @@ namespace Joker.LearnBlazor.WebService.Repository
 {
     public class ProductRepositoryMssql : IProduct
     {
+        private readonly ICategory _category;
+        public ProductRepositoryMssql(ICategory category)
+        {
+            _category = category;
+        }
+
         public void Add(Product model)
         {
             SqlSugarHelper.Db.Insertable(model).ExecuteCommand();
         }
 
+        /// <summary>
+        /// 统计各个分类商品的数量
+        /// </summary>
+        /// <param name="caid"></param>
+        /// <returns></returns>
         public int CalcCount(int caid)
         {
-            return SqlSugarHelper.Db.Queryable<Product>().Where(pro => pro.CategoryId == caid).Count();
+            Category ca = _category.GetModel(caid);
+            string tmp = string.Empty;
+            if (ca.ParentId == 0)
+            {
+                // 第一级分类 ,1,
+                tmp = $",{ca.CategoryId},";
+            }
+            else
+            {
+                tmp = ca.CategoryPath + ca.CategoryId + ",";
+            }
+            // 下级分类的商品数
+            int nextLevelCount = SqlSugarHelper.Db.Queryable<Product>().Where(pro => pro.Category.CategoryPath.StartsWith(tmp)).Count();
+            // 本级分类的商品数
+            int curLevelCount = SqlSugarHelper.Db.Queryable<Product>().Where(pro => pro.CategoryId == caid).Count();
+            return nextLevelCount + curLevelCount;
         }
 
         public void Delete(int id)
