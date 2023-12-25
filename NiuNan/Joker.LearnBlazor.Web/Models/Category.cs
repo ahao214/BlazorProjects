3b6fc437b1260f1536ce8ca3d3bed3c0304b8da5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-25 12:28:27 +0800

    商品分类

---
+++ NiuNan/Joker.LearnBlazor.Web/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace Joker.LearnBlazor.Web.Models
+{
+    /// <summary>
+    /// 商品分类
+    /// </summary>
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int ParentId { get; set; }
+
+        public List<Category> Items { get; set; }
+
+    }
+}
