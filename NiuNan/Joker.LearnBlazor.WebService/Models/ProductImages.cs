ea0d5bbc078331a0714ef23c0a1b4caa2be106f7
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-26 21:13:19 +0800

    ProductImages

---
+++ NiuNan/Joker.LearnBlazor.WebService/Models/ProductImages.cs
@@ -0,0 +1,21 @@
+﻿namespace Joker.LearnBlazor.WebService.Models
+{
+    /// <summary>
+    /// 商品图片
+    /// </summary>
+    public class ProductImages
+    {
+        /// <summary>
+        /// 主键自增
+        /// </summary>
+        public int ImageId { get; set; }
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int ProductId { get; set; }
+        /// <summary>
+        /// 图片标题
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
