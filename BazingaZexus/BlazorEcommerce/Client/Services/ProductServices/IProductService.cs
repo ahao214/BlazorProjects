f535c17dd9c954da848d5e8d30aa94f9ddf38bc4
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 12:03:43 +0800

    Client 端添加获取产品数量

---
+++ BazingaZexus/BlazorEcommerce/Client/Services/ProductServices/IProductService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductServices
+{
+    public interface IProductService
+    {
+        List<Product> Prducts { get; set; }
+        Task GetProducts { get; set; }
+    }
+}
