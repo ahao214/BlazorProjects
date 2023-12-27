2ecfc8893dd0c61bf12c942ec40722686b1d8591
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 14:49:14 +0800

    获取所有商品分类

---
+++ BazingaZexus/BlazorEcommerce/Server/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BlazorEcommerce.Server.Services.CategoryServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        /// <summary>
+        /// 获取所有商品分类
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
+        {
+            var result = await _categoryService.GetCategories();
+            return Ok(result);
+        }
+
+    }
+}
