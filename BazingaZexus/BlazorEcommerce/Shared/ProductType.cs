5eaa56b2ef47b4ffa20a07513355e9eb5521c757
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-27 15:45:33 +0800

    add 类

---
+++ BazingaZexus/BlazorEcommerce/Shared/ProductType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    /// <summary>
+    /// 商品种类
+    /// </summary>
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
