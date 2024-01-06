6906184345b4bdf9752bc1864af9e7b9349d06be
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-06 22:00:34 +0800

    add UserRegister

---
+++ BazingaZexus/BlazorEcommerce/Shared/UserRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class UserRegister
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+
+
+    }
+}
