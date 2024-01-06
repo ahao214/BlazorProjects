5c2de65de7ef7c7e3ab3c5d9e238c0881f66d19d
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-06 18:28:04 +0800

    add 购物车计数器

--- BazingaZexus/BlazorEcommerce/Client/Program.cs
+++ BazingaZexus/BlazorEcommerce/Client/Program.cs
@@ -6,10 +6,14 @@ using BlazorEcommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerce.Client.Services.CategoryServices;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
