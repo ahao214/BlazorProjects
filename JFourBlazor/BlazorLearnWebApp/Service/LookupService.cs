103f5a441e5bcb4bc0d9e68706126c0c7e8745f7
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-13 12:58:27 +0800

    用户管理界面，绑定角色信息

---
+++ JFourBlazor/BlazorLearnWebApp/Service/LookupService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorLearnWebApp.Entity;
+using BootstrapBlazor.Components;
+
+namespace BlazorLearnWebApp.Service
+{
+    public class LookupService : ILookupService
+    {
+        public IEnumerable<SelectedItem>? GetItemsByKey(string? key)
+        {
+            // 用户管理界面，绑定角色信息
+            if (key == "role")
+            {
+                return RoleEntity.Select.ToList(x => new SelectedItem(x.Id.ToString(), x.RoleName!));
+            }
+            return null;
+        }
+    }
+}
