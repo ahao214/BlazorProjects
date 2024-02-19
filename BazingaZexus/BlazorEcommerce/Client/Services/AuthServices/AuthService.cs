4f607c2f3b3d5845dac5476fc90277a8130591bc
liupenghao@fulan.com.cn <haochen214@163.com>
2024-02-19 15:02:13 +0800

    authservice

--- BazingaZexus/BlazorEcommerce/Client/Services/AuthServices/AuthService.cs
+++ BazingaZexus/BlazorEcommerce/Client/Services/AuthServices/AuthService.cs
@@ -10,10 +10,27 @@ namespace BlazorEcommerce.Client.Services.AuthServices
             _http = http;
         }
 
+        /// <summary>
+        /// 用户注册
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         public async Task<ServiceResponse<int>> Register(UserRegister request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
             return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<ServiceResponse<string>> Login(UserLogin request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/login", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<string>>();
 
         }
     }
--- BazingaZexus/BlazorEcommerce/Client/Services/AuthServices/IAuthService.cs
+++ BazingaZexus/BlazorEcommerce/Client/Services/AuthServices/IAuthService.cs
@@ -3,6 +3,8 @@
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
-        
+
+        Task<ServiceResponse<string>> Login(UserLogin request);
+
     }
 }
